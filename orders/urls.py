c87ad4fc11a5a78311103f21aa133703b10c75e6
Jganyo-dotcom <elikemejay@gmail.com>
2025-06-18 16:12:29 +0000

    removdhim

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = 'index'),
+    
     path('submit_order/', views.submit_order, name='submit_order'),
     
 
