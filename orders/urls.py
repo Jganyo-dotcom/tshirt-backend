ceb978d9663922c95739e4ebf1df790d423759c1
Jganyo-dotcom <elikemejay@gmail.com>
2025-06-22 04:08:49 +0000

    setup

--- orders/urls.py
+++ orders/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('submit_specific_order/', views.submit_specific_order, name='submit_specific_order')
+     path('submit_specific_order/', views.submit_specific_order, name='submit_specific_order'),
     path('submit_order/', views.submit_order, name='submit_order'),
    
 ]
