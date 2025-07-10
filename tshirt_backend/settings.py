7dd26554affbaeab2d7ae7f2b790b91cf14c8fc0
Jganyo-dotcom <elikemejay@gmail.com>
2025-07-10 10:45:59 +0000

    try

--- tshirt_backend/settings.py
+++ tshirt_backend/settings.py
@@ -133,7 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOWED_ORIGINS = [
     "https://jganyo-dotcom.github.io", 
-     "https://kagoedition.netlify.app" # Your GitHub Pages site
+     "https://kagoedition.netlify.app"
+       "http://127.0.0.1:5500",
+ # Your GitHub Pages site
 ]
 
 
